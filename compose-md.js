a0f05ff58fa1aeab704db22565882d9ff62bd579
Yannick Lohse <yannick.lohse@caldera.com>
2013-08-14 14:13:27 +0200

    Bugfix fore firefox

--- compose-md.js
+++ compose-md.js
@@ -110,7 +110,7 @@
 					this.positionCarret($p[0], 0);
 				}
 			}
-			else if (selection.focusOffset < selection.anchorNode.innerText.length){
+			else if (selection.focusOffset < $(selection.anchorNode).text().length){
 				//caret wesn't at th end, try to reposition it where it used to be
 				var newOffset = ComposeMarkdown.matchOffset($html, destination, 0);
 				this.positionCarret(newOffset.node, newOffset.offset);
